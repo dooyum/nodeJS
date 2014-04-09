25cea24a331fbcf5d0a4fdbf25a17556d60d961f
Dooyum Malu <dmalu@andrew.cmu.edu>
2014-04-08 17:12:23 -0700

    Visualization works well

--- visualization/mock1/routes/socket.js
+++ visualization/mock1/routes/socket.js
@@ -1,24 +1,30 @@
 /*
  * Serve content over a socket
  */
-var backendSocket = require('net').Socket();
-var carrier = require('carrier');
+var backendSocketCPU = require('net').Socket();
+var carrierCPU = require('carrier');
+
+var backendSocketGPU = require('net').Socket();
+var carrierGPU = require('carrier');
 
 var cpuCount = 0;
+var gpuCount = 0;
+var speedFactor = 0.5;
 
 
 module.exports = function (socket) {
 	//when connection to web socket has been made is when we should start piping content
 	socket.on('init', function (data) {
-		backendSocket.connect(11530, "localhost");	//Connect to socket
+		backendSocketCPU.connect(7000, "localhost");	//Connect to socket
 
-		backendSocket.on("error", function(error){
+		backendSocketCPU.on("error", function(error){
 			console.log(error);
 		}); 
 
-		carrier.carry(backendSocket, function(line){
+		carrierCPU.carry(backendSocketCPU, function(line){
 			try {
 			  line = JSON.parse(line);
+			  console.log(line);
 			  socket.emit('cpu:data', line);
 			  cpuCount++;
 			} catch (e) {
@@ -26,16 +32,42 @@ module.exports = function (socket) {
 			}
 		});
 
-		setInterval(function () {
-			backendSocket.write("testing" + (new Date()).toString());
-		}, 1000);
+		/*setInterval(function () {
+			backendSocketCPU.write("testing" + (new Date()).toString());
+		}, 3000);*/
+
+		backendSocketGPU.connect(6000, "localhost");	//Connect to socket
+
+		backendSocketGPU.on("error", function(error){
+			console.log(error);
+		}); 
+
+		carrierGPU.carry(backendSocketGPU, function(line){
+			try {
+			  line = JSON.parse(line);
+			  console.log(line);
+			  socket.emit('gpu:data', line);
+			  gpuCount++;
+			} catch (e) {
+			  console.error("Parsing error:", e); 
+			}
+		});
+
+		/*setInterval(function () {
+			backendSocketGPU.write("testing" + (new Date()).toString());
+		}, 3000);*/
 
 		setInterval(function () {
 			socket.emit('cpu:count', {
 		      count: cpuCount
 		    });
 			cpuCount = 0;
-		}, 1000);
+
+			socket.emit('gpu:count', {
+		      count: gpuCount
+		    });
+			gpuCount = 0;
+		}, 1000 * speedFactor);
 	});
 
 };
